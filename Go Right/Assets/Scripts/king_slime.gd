1cdddb6b16e3954aa17dc8b1b9c3d6e13538a760
tsjngo <tsjngo@gmail.com>
2026-02-20 01:49:57 -0800

    Update World king slime

--- Go Right/Assets/Scripts/king_slime.gd
+++ Go Right/Assets/Scripts/king_slime.gd
@@ -5,14 +5,22 @@ extends CharacterBody2D
 var direction: Vector2 = Vector2.DOWN
 var jump = false
 var target
+var baby_slimes = preload("res://Assets/Enemies/slime.tscn")
 
 func _on_enemy_hitbox_take_damage(damage: Variant) -> void:
 	HEALTH -= damage
 
 
 func _process(_delta):
-	if HEALTH == 0:
+	if HEALTH <= 0:
+		for i in range(5):
+			var baby_slime = baby_slimes.instantiate()
+			baby_slime.global_position.x = self.global_position.x + randi_range(-20, 21)
+			baby_slime.global_position.y = self.global_position.y + randi_range(-20, 21)
+			baby_slime.scale = Vector2(3, 3)
+			get_parent().add_child(baby_slime)
 		queue_free()
+		
 	if jump:
 		direction = (target.global_position - global_position).normalized()
 		velocity = round(direction * SPEED * randf_range(0.8, 1.2))
